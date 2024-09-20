4fd73d3b678a1f879872ae26dce132477065414a
PittnoiR <omargarza394@gmail.com>
2024-09-20 16:49:53 -0700

    createComponent(Header,Media)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+import { Media } from "./components/Media"
+import './styles/app.css'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Media/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Media.jsx
@@ -0,0 +1,19 @@
+import '../styles/media.css'
+
+export const Media =()=>{
+    return(
+        <section className="Media" >
+            <figure className='Media-frame'>
+            <iframe src="https://mega.nz/embed/!chFBGa4S!39M4aKbD2sssQ_wXNvZAQAejoqwG0VqTE5Z8AjEx0x4" allowFullScreen  aria-controls="1" allow="autoplay; encrypted-media"></iframe>
+            </figure>
+            <div className="Media-reframe">
+                <a href="#">clickeable</a>
+                <a href="#">clickeable</a>
+                <a href="#">clickeable</a>
+                <a href="#">clickeable</a>
+            </div>
+        </section>
+
+        
+    );
+}
