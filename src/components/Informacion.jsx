90656693a88d56c9146215b24ed6417ab91f7040
PittnoiR <omargarza394@gmail.com>
2024-09-20 18:50:39 -0700

    creat Information component complet/+Hook+API+backend

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,35 +1,38 @@
+import '../styles/informacion.css'
+
 export const Informacion = ()=>{
     return(
         <section className="Inforanime">
             <div className="Inforanime-punteo">
                 <figure>
-                    <img src="/public/svgs/star-icon.svg" alt="star" />
-                    <img src="/public/svgs/star-icon.svg" alt="star" />
-                    <img src="/public/svgs/star-icon.svg" alt="star" />
-                    <img src="/public/svgs/star-icon.svg" alt="star" />
-                    <img src="/public/svgs/star-icon.svg" alt="star" />
+                    <img src="/svgs/star-icon.svg" alt="star" />
+                    <img src="/svgs/star-icon.svg" alt="star" />
+                    <img src="/svgs/star-icon.svg" alt="star" />
+                    <img src="/svgs/star-icon.svg" alt="star" />
+                    <img src="/svgs/star-icon.svg" alt="star" />
                 </figure>
                 <div>
-                    <p>9999</p>
+                    <p><strong>Punteo:</strong>9999</p>
                 </div>
             </div>
-            <h1>Sword Art  Online</h1>
+            <h1><strong>Nombre Original:</strong> Sward art online</h1>
             <section className="Inforanime-info">
-                <div>
-                    <h2><strong>Nombre Original:</strong> Sward art online</h2>
-                    <h2>Información</h2>
+                <div id='info'>
+                    <h2>Sword Art  Online</h2>
+                    <h5>Información</h5>
                     <p><strong>Genero:</strong> Acción, Aventura, Fantasía, Romance</p>
                     <div  className="Inforanime-info-text">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolore facilis consequuntur esse, recusandae tempora nam libero ducimus facere doloribus voluptatem veniam repellat ex aut dignissimos vel architecto pariatur accusantium.</p>
                     </div>
                 </div>
                 <div className="Inforanime-info-img">
-                    <img src="" alt="sao" />
+                    <figure>
+
+                    </figure>
                     <div>
-                        <p><strong>Temporadas:</strong> 4</p>
-                        <p><strong>Capitulos:</strong> 100</p>
-                        <p><strong>Estado:</strong> En emisión</p>
-                        <p><strong>estreno:</strong> 10/10/2010</p>
+                        <p><strong>Tem/</strong> 4</p>
+                        <p><strong>Cap/</strong> 100</p>
+                        <p>En emisión</p>
                     </div>
                 </div>
 
